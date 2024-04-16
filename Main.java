5b2de07899ad89b393d5f1504c0bb4fc11b01222
Archar1os <148558039+Archar1os@users.noreply.github.com>
2024-04-16 12:48:52 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,6 +4,33 @@ public class Main
 {
     public static void main(String[] args)
     {
-        User();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Menu");
+        System.out.println("View Books(1)");
+        System.out.println("Add Books (2)");
+        System.out.println("Delete    (3)");
+        System.out.println("Exit      (4)");
+        System.out.println(":");
+        int user = input.nextInt();
+        if (user == 1)
+        {
+            System.out.println("ok1");
+        }
+        else if (user == 2)
+        {
+            System.out.println("ok2");
+        }
+        else if (user == 3)
+        {
+            System.out.println("ok3");
+        }
+        else if (user == 4)
+        {
+            System.out.println("ok4");
+        }
+        else
+        {
+            System.out.println("error");
+        }
     }
 }
